1eec0f82dabbda725d877ba8c4a094a45220d92b
leygallego <leyder.gallego@gmail.com>
2022-02-27 08:58:45 -0500

    Avance

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer(state, {type, payload}) {
     switch (type) {
         case ACTIONS.ADD_DIGIT:
+            if (state.overwrite){
+                return{
+                    ...state,
+                    currentOperand: payload.digit,
+                    overwrite: false
+                }
+            }
             if(payload.digit === "0" && state.currentOperand ===  "0" ) {
                 return state
             }
@@ -52,7 +59,41 @@ function reducer(state, {type, payload}) {
         }    
         case ACTIONS.CLEAR:{
             return {}
-        }    
+        } 
+        case ACTIONS.DELETE_DIGIT:{
+            if (state.overwrite){
+                return{
+                    ...state,
+                    overwrite: false,
+                    currentOperand: null
+                }
+            }
+            if (state.currentOperand == null) return state
+            if (state.currentOperand.length === -1){
+                return {...state, currentOperand: null}
+            }
+            return{
+                ...state,
+                currentOperand: state.currentOperand.slice(0, -1)
+            }
+        }
+        case ACTIONS.EVALUATE:{
+            if(
+                state.operation == null ||
+                state.currentOperand == null ||
+                state.previousOperand == null
+            ){
+                return state
+            }
+
+            return{
+                ...state,
+                overwrite: true,
+                previousOperand: null,
+                operation: null,
+                currentOperand: evaluate(state)
+            }
+        }   
             
     }
     
@@ -80,6 +121,16 @@ function evaluate({currentOperand, previousOperand, operation}) {
     return computation.toString()
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+    maximumFractionDigits: 0,
+  })
+  function formatOperand(operand) {
+    if (operand == null) return
+    const [integer, decimal] = operand.split(".")
+    if (decimal == null) return INTEGER_FORMATTER.format(integer)
+    return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+  }
+
 const App = () => {
 
     const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
@@ -88,11 +139,11 @@ const App = () => {
     return (
         <div className='calculator-grid' >
             <div className="output">
-                <div className="previous-operand"> {previousOperand} {operation} </div>
-                <div className="current-operand">{currentOperand}</div>
+                <div className="previous-operand"> {formatOperand(previousOperand)} {operation} </div>
+                <div className="current-operand">{formatOperand(currentOperand)}</div>
             </div>
             <button className="span-two" onClick={()=> dispatch({type: ACTIONS.CLEAR})}>AC</button>
-            <button>DEL</button>
+            <button onClick={()=> dispatch({type: ACTIONS.DELETE_DIGIT})} >DEL</button>
              <OperationButton operation="÷" dispatch={dispatch}/>
 
              <DigitButton digit="1" dispatch={dispatch}/>
@@ -109,7 +160,7 @@ const App = () => {
              <OperationButton operation="-" dispatch={dispatch}/>
              <DigitButton digit="." dispatch={dispatch}/>
             <DigitButton digit="0" dispatch={dispatch}/>
-            <button className="span-two">=</button>
+            <button className="span-two" onClick={()=> dispatch({type: ACTIONS.EVALUATE})} >=</button>
 
         </div>
     );
